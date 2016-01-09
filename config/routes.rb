52c7d39ecf320060cfcb2dc4f75a601fd70723e4
violetaria <terri.chu@outlook.com>
2016-01-09 16:10:00 -0500

    user routes for login, register, update, destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  post 'users/register', to: 'users#create'
+  post 'users', to: 'users#login'
+  patch 'users', to: 'users#update'
+  delete 'users', to: 'users#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
