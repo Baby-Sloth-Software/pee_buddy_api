ce1282a3eea42574b61a2e5b836fe58164b2be6a
violetaria <terri.chu@outlook.com>
2016-01-09 16:22:43 -0500

    added length validation to username

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
 
   validates_presence_of :email, :username, :password_digest
   validates_uniqueness_of :email, :username
+  validates_length_of :username, within: 4..20, too_long: 'pick a shorter username', too_short: 'pick a longer username'
   validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
 
   def ensure_auth_token
