f1539c57ada95b7982045a9405b193a537626d44
violetaria <terri.chu@outlook.com>
2016-01-28 16:15:41 -0500

    made all columns null false, changed place_id to string

--- app/controllers/api/v1/rater_controller.rb
+++ app/controllers/api/v1/rater_controller.rb
@@ -1,7 +1,8 @@
 class RaterController < ApplicationController
-  before_action authenticate_user!
+  before_action :authenticate_user!
 
   def create
+    binding.pry
     obj = params[:klass].classify.constantize.find(params[:id])
     obj.rate params[:score].to_f, current_user, params[:dimension]
 
