512d87466fad26b35b4da7a3329d2512834ca3de
violetaria <terri.chu@outlook.com>
2016-01-28 17:11:05 -0500

    add name column to locations table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160128211335) do
+ActiveRecord::Schema.define(version: 20160128220620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20160128211335) do
     t.float    "lng",        null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name",       null: false
   end
 
   create_table "overall_averages", force: :cascade do |t|
