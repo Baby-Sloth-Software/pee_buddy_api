d01ec143f3a1c0de134810a4d3bfc83963c0b695
violetaria <terri.chu@outlook.com>
2016-02-12 09:00:45 -0500

    test new hooks

---
+++ app/controllers/api/v1/locations_controller.rb
@@ -0,0 +1,26 @@
+module Api
+  module V1
+    class LocationsController < ApplicationController
+      before_action :authenticate_user!
+
+      def create
+        locations = params["locations"]
+        @data = locations.map do |location|
+          Location.find_or_create_by!(
+              place_id: location["place_id"],
+              lat: location["location"]["lat"].to_f,
+              lng: location["location"]["lng"].to_f,
+              name: location["name"]
+          )
+        end
+        render "create.json.jbuilder", status: :ok
+      end
+
+      def show
+
+        binding.pry
+      end
+
+    end
+  end
+end
