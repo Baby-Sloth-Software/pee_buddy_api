32d4c849b46f8e01976af226667bc5eb0eb88af0
violetaria <terri.chu@outlook.com>
2016-02-12 09:04:20 -0500

    test commit

--- app/controllers/api/v1/rater_controller.rb
+++ app/controllers/api/v1/rater_controller.rb
@@ -1,11 +1,15 @@
-class RaterController < ApplicationController
-  before_action :authenticate_user!
+module Api
+  module V1
+    class RaterController < ApplicationController
+      before_action :authenticate_user!
 
-  def create
-    binding.pry
-    obj = params[:klass].classify.constantize.find(params[:id])
-    obj.rate params[:score].to_f, current_user, params[:dimension]
+      def create
+        binding.pry
+        obj = params[:klass].classify.constantize.find(params[:id])
+        obj.rate params[:score].to_f, current_user, params[:dimension]
 
-    render :json => true
+        render :json => true
+      end
+    end
   end
 end
